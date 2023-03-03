2284644e21c82a25eb442bfc965f6651b500f60e
Possathorn Sujipisut <78402218+ThePun123@users.noreply.github.com>
2023-03-03 19:20:50 +0700

    Create tourAssignment.java

---
+++ tourAssignment.java
@@ -0,0 +1,16 @@
+/*
+Possathorn Sujipisut 6480274
+ */
+package Project2_6480929;
+
+public class tourAssignment {
+    String path = "src/main/java/Project2_6480929";
+    
+    
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
